806416cdc84caaa19a5e9ca625044f08c3859465
mrjohnming <john.ngai@hotmail.com>
2022-06-05 15:25:44 -0400

    Add states

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
+import { useEffect, useState } from 'react';
 import './style.css';
 
 export default function App() {
+  // States
+  const [page, setPage] = useState('START');
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    // Get 5 questions from the Open Trivia DB API.
+    fetch('https://opentdb.com/api.php?amount=5&difficulty=easy&type=multiple')
+      .then(res => res.json())
+      .then(data => setQuestions(data.results));
+  }, []);
+
   return (
     <main>
-      <h1>App Component</h1>
+      {page === 'START' &&
+        <div className='start-page'>
+          <h1 className='title'>Quizzical</h1>
+          <p className='description'>Some description if needed</p>
+          <button className='button--start'
+            onClick={() => setPage('QUIZ')}
+          >Start Quiz</button>
+        </div>}
+
+      {page === 'QUIZ' &&
+        <button onClick={() => setPage('START')}>Go Back</button>
+      }
     </main>
   );
 }
