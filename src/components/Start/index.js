dafae3493b5565a808d8791007b4a3b3a0de087e
mrjohnming <john.ngai@hotmail.com>
2022-06-13 19:56:41 -0400

    Enable button onClick property

--- src/components/Start/index.js
+++ src/components/Start/index.js
@@ -1,15 +1,12 @@
 import './index.css';
 
 export default function Start(props) {
-  const { setPage } = props.setPage;
-
-
   return (
     <section className='start-page'>
       <h1 className='title'>Quizzical</h1>
       <p className='description'>Some description if needed</p>
       <button className='button--start'
-        /*onClick={() => setPage('QUIZ')}*/
+        onClick={props.setPage}
       >Start Quiz</button>
     </section>
   );
