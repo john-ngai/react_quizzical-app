50ec3d18a317afefa8c72d93571f25e0e05c2a02
mrjohnming <john.ngai@hotmail.com>
2022-06-15 17:14:36 -0400

    Import component - Question

--- src/components/Quiz/index.js
+++ src/components/Quiz/index.js
@@ -1,8 +1,10 @@
+import Question from './Question';
 import './index.css';
 
 export default function Quiz() {
   return (
-    <section className='quiz-page'> 
+    <section className='quiz-page'>
+      <Question />
       <button className='button--check'>Check Answers</button>
     </section>
   );
