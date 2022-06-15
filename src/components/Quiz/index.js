e0330c26b9ba7e61f3a175ef8be66ee8ad67beff
mrjohnming <john.ngai@hotmail.com>
2022-06-15 17:10:59 -0400

    Create component - Quiz

---
+++ src/components/Quiz/index.js
@@ -0,0 +1,9 @@
+import './index.css';
+
+export default function Quiz() {
+  return (
+    <section className='quiz-page'> 
+      <button className='button--check'>Check Answers</button>
+    </section>
+  );
+}
