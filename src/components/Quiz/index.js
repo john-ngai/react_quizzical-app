7687549ea809cc2549bac9675d7c4570727bdf5e
mrjohnming <john.ngai@hotmail.com>
2022-06-20 12:11:53 -0400

    Move the Question component property formatting implementation into the App component

--- src/components/Quiz/index.js
+++ src/components/Quiz/index.js
@@ -1,26 +1,20 @@
-import { useState } from 'react';
-import { decode } from 'html-entities';
 import { nanoid } from 'nanoid';
 import Question from './Question';
 import './index.css';
 
+// APP > QUIZ
 export default function Quiz(props) {
 const questionElements = props.questions.map(element => {
-    let { question, correct_answer, incorrect_answers } = element;
-    // Convert HTML entities into characters.
-    question = decode(question);
-    correct_answer = decode(correct_answer);
-    incorrect_answers = incorrect_answers.map(answer => decode(answer));
-    // Add & shuffle all answers to the choices array.
-    const choices = incorrect_answers;
-    choices.push(correct_answer);
-    choices.sort(() => Math.random() - 0.5);
+    const { question, choices, correct_answer, selected } = element;
     // Return each element as a Question component.
     return (
       <Question
         key={nanoid()}
         question={question}
         choices={choices}
+        correct_answer={correct_answer}
+        selected={selected}
+        updateSelected={props.updateSelected}
       />
     );
   });
