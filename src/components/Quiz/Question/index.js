231e826bc95f0c6fc7b15f8fb011e407c2b3049e
mrjohnming <john.ngai@hotmail.com>
2022-06-21 18:01:39 -0400

    Add conditional button classNames

--- src/components/Quiz/Question/index.js
+++ src/components/Quiz/Question/index.js
@@ -1,22 +1,45 @@
 import './index.css';
 
 export default function Question(props) {
-  const { question, choices } = props
-
+  const { question, choices, correct_answer, selected, updateSelected } = props
   return (
     <div>
       <h3 className='question-title'>{question}</h3>
-      <button className="button--option"
-        onClick={event => console.log(event.target.innerText)}
+      <button
+        className={selected === choices[0] ?
+          'button--option selected' : 'button--option'
+        }
+        onClick={event => {
+          const choice = event.target.innerText;
+          return updateSelected(question, choice);
+        }}
       >{choices[0]}</button>
-      <button className="button--option"
-        onClick={event => console.log(event.target.innerText)}
+      <button
+        className={selected === choices[1] ?
+          'button--option selected' : 'button--option'
+        }
+        onClick={event => {
+          const choice = event.target.innerText;
+          return updateSelected(question, choice);
+        }}
       >{choices[1]}</button>
-      <button className="button--option"
-        onClick={event => console.log(event.target.innerText)}
+      <button
+        className={selected === choices[2] ?
+          'button--option selected' : 'button--option'
+        }
+        onClick={event => {
+          const choice = event.target.innerText;
+          return updateSelected(question, choice);
+        }}
       >{choices[2]}</button>
-      <button className="button--option"
-        onClick={event => console.log(event.target.innerText)}
+      <button
+        className={selected === choices[3] ?
+          'button--option selected' : 'button--option'
+        }
+        onClick={event => {
+          const choice = event.target.innerText;
+          return updateSelected(question, choice);
+        }}
       >{choices[3]}</button>
       <div className="line" />
     </div>
