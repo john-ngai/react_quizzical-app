34907713e6adf7e2a81d4c2622e1e212a5ca3b84
mrjohnming <john.ngai@hotmail.com>
2022-06-22 17:05:57 -0400

    Add more conditionals to the button styling

--- src/components/Quiz/Question/index.js
+++ src/components/Quiz/Question/index.js
@@ -1,44 +1,69 @@
 import './index.css';
 
 export default function Question(props) {
-  const { question, choices, correct_answer, selected, updateSelected } = props
+  const { question, choices, correct_answer, selected, updateSelected, page } = props
+
+  const quizbuttonClass = (selected, choice) => {
+    return selected === choice ? 'button--option selected' : 'button--option';
+  }
+
+  const answersButtonClass = (selected, choice, correct_answer) => {
+    if (selected === correct_answer && selected === choice) {
+      return 'button--result correct';
+    } else if (selected !== correct_answer && selected === choice) {
+      return 'button--result incorrect';
+    } else if (choice === correct_answer) {
+      return 'button--result correct';
+    } else {
+      return 'button--result';
+    }
+  }
+
   return (
     <div>
       <h3 className='question-title'>{question}</h3>
       <button
-        className={selected === choices[0] ?
-          'button--option selected' : 'button--option'
+        className={page === 'QUIZ' ?
+          quizbuttonClass(selected, choices[0]) :
+          page === 'ANSWERS' ?
+            answersButtonClass(selected, choices[0], correct_answer) : ''
         }
         onClick={event => {
           const choice = event.target.innerText;
-          return updateSelected(question, choice);
+          return page === 'QUIZ' ? updateSelected(question, choice) : null;
         }}
       >{choices[0]}</button>
       <button
-        className={selected === choices[1] ?
-          'button--option selected' : 'button--option'
+        className={page === 'QUIZ' ?
+          quizbuttonClass(selected, choices[1]) :
+          page === 'ANSWERS' ?
+            answersButtonClass(selected, choices[1], correct_answer) : ''
         }
         onClick={event => {
           const choice = event.target.innerText;
-          return updateSelected(question, choice);
+          return page === 'QUIZ' ? updateSelected(question, choice) : null;
         }}
       >{choices[1]}</button>
       <button
-        className={selected === choices[2] ?
-          'button--option selected' : 'button--option'
+        className={page === 'QUIZ' ?
+          quizbuttonClass(selected, choices[2]) :
+          page === 'ANSWERS' ?
+            answersButtonClass(selected, choices[2], correct_answer) : ''
         }
         onClick={event => {
           const choice = event.target.innerText;
-          return updateSelected(question, choice);
+          return page === 'QUIZ' ? updateSelected(question, choice) : null;
         }}
       >{choices[2]}</button>
       <button
-        className={selected === choices[3] ?
-          'button--option selected' : 'button--option'
+        className={page === 'QUIZ' ?
+          quizbuttonClass(selected, choices[3]) :
+          page === 'ANSWERS' ?
+            answersButtonClass(selected, choices[3], correct_answer) : ''
         }
         onClick={event => {
           const choice = event.target.innerText;
-          return updateSelected(question, choice);
+          return page === 'QUIZ' ? updateSelected(question, choice) : null;
         }}
       >{choices[3]}</button>
       <div className="line" />
